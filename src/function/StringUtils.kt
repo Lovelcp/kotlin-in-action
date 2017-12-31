7ce796a1a9c4ccad2fd44dbef3a840e18f18f078
kissrain <cjyang1123@gmail.com>
2017-12-31 17:15:33 +0800

    add extension functions test

---
+++ src/function/StringUtils.kt
@@ -0,0 +1,3 @@
+package function
+
+fun String.lastChar(): Char = get(length - 1)
---
+++ src/function/StringUtilsJavaTest.java
@@ -0,0 +1,7 @@
+package function;
+
+public class StringUtilsJavaTest {
+    public static void main(String[] args) {
+        System.out.println(StringUtilsKt.lastChar("abcd"));
+    }
+}
