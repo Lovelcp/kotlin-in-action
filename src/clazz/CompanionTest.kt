435da9fb6ed1b5ec859292e1137c09dac819a1f6
kissrain <cjyang1123@gmail.com>
2018-01-04 23:47:23 +0800

    add companion test

---
+++ src/clazz/CompanionTest.kt
@@ -0,0 +1,13 @@
+package clazz
+
+class A {
+    companion object {
+        fun bar() {
+            println("Companion object called")
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    A.bar()
+}
