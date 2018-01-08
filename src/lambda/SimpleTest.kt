27a7fe5a661231e0a1c7036473206201e234528b
kissrain <cjyang1123@gmail.com>
2018-01-08 23:24:50 +0800

    lambda修改局部变量

--- src/lambda/SimpleTest.kt
+++ src/lambda/SimpleTest.kt
@@ -2,6 +2,25 @@ package lambda
 
 import basic.Person
 
+fun printMessagesWithPrefix(messages: Collection<String>, prefix: String) {
+    messages.forEach {
+        println("$prefix $it")
+    }
+}
+
+fun printProblemCounts(responses: Collection<String>) {
+    var clientErrors = 0
+    var serverErrors = 0
+    responses.forEach {
+        if (it.startsWith("4")) {
+            clientErrors++
+        } else if (it.startsWith("5")) {
+            serverErrors++
+        }
+    }
+    println("$clientErrors client errors, $serverErrors server errors")
+}
+
 fun main(args: Array<String>) {
     val sum = { x: Int, y: Int -> x + y }
     { println(42) }()
@@ -13,4 +32,10 @@ fun main(args: Array<String>) {
     println(people.maxBy { p: Person -> p.age }) // 最完整的形式
     println(people.maxBy { p -> p.age }) // 省略Person类型的定义
     println(people.maxBy { it.age }) // 通过it来指代person对象
+
+    val errors = listOf("403 Forbidden", "404 Not Found")
+    printMessagesWithPrefix(errors, "Error:")
+
+    val responses = listOf("200 OK", "418 I'm a teapot", "500 Internal Server Error")
+    printProblemCounts(responses)
 }
