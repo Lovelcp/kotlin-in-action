5c0f753f1016b31f942e001de61c0d6f957f846b
kissrain <cjyang1123@gmail.com>
2018-01-29 23:22:02 +0800

    learn sequence

--- src/lambda/CollectionTest.kt
+++ src/lambda/CollectionTest.kt
@@ -24,4 +24,14 @@ fun main(args: Array<String>) {
 
     val books = listOf(Book("A", listOf("1")), Book("B", listOf("2")), Book("C", listOf("2", "3")))
     println(books.flatMap { it.authors }.toSet())
+
+    // 不会执行print，因为是惰性操作
+    listOf(1, 2, 3, 4).asSequence().map { println("map($it) "); it * it }.filter { println("filter($it)"); it % 2 == 0 }
+
+    // 只有执行了toList，从序列转换为集合的时候，才会打印信息
+    listOf(1, 2, 3, 4).asSequence().map { println("map($it) "); it * it }.filter { println("filter($it)"); it % 2 == 0 }.toList()
+
+    // 通过比较可以清晰地看到序列和非序列对元素的操作顺序是不一样的
+    listOf(1, 2, 3, 4).map { println(it);it * it }.find { println(it);it > 3 }
+    listOf(1, 2, 3, 4).asSequence().map { println(it);it * it }.find { println(it);it > 3 }
 }
