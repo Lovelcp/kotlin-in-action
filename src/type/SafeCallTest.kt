54746f8cef79d36f1b6a2989a3f5a6b7a31d93e7
kissrain <cjyang1123@gmail.com>
2018-02-15 15:38:52 +0800

    add safe call test

---
+++ src/type/SafeCallTest.kt
@@ -0,0 +1,33 @@
+package type
+
+fun printAllCaps(s: String?) {
+    val allCaps: String? = s?.toUpperCase()
+    println(allCaps)
+}
+
+class Employee(val name: String, val manager: Employee?)
+
+fun managerName(employee: Employee): String? = employee.manager?.name
+
+class Address(val streetAddress: String, val zipCode: Int, val city: String, val country: String)
+class Company(val name: String, val address: Address?)
+class Person(val name: String, val company: Company?)
+
+fun Person.countryName(): String {
+    val country = this.company?.address?.country
+    return if (country != null) country else "Unknown"
+}
+
+fun main(args: Array<String>) {
+    printAllCaps("abc")
+    printAllCaps(null)
+
+    val ceo = Employee("Da Boss", null)
+    val developer = Employee("Bob Smith", ceo)
+    println(managerName(developer))
+    println(managerName(ceo))
+
+    val person = Person("Dmitry", null)
+    println(person.countryName())
+}
+
