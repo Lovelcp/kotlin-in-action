951a39d4600ad1202ed6671f9401f25ee8580c50
kissrain <cjyang1123@gmail.com>
2018-02-15 14:30:24 +0800

    add generate sequence demo

---
+++ src/lambda/SequenceTest.kt
@@ -0,0 +1,7 @@
+package lambda
+
+fun main(args: Array<String>) {
+    val naturalNumbers = generateSequence(0) { it + 1 }
+    val numbersTo100 = naturalNumbers.takeWhile { it <= 100 }
+    println(numbersTo100.sum())
+}
