c5fa26fd9bc5615056cfcbc266619593001f8598
kissrain <cjyang1123@gmail.com>
2018-02-19 20:16:39 +0800

    iterator test

---
+++ src/operator/IteratorTest.kt
@@ -0,0 +1,19 @@
+package operator
+
+import java.time.LocalDate
+
+operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> = object : Iterator<LocalDate> {
+    var current = start
+
+    override fun hasNext(): Boolean = current <= endInclusive
+
+    override fun next(): LocalDate = current.apply { current = plusDays(1) }
+}
+
+fun main(args: Array<String>) {
+    val newYear = LocalDate.ofYearDay(2017, 1)
+    val daysOff = newYear.minusDays(1)..newYear
+    for (dayOff in daysOff) {
+        println(dayOff)
+    }
+}
