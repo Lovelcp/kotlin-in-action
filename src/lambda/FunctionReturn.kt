a5bded5b7f9a58ed69296fbcd8f3063b86d580c1
kissrain <cjyang1123@gmail.com>
2018-02-23 10:48:53 +0800

    lambda消除冗余函数

--- src/lambda/FunctionReturn.kt
+++ src/lambda/FunctionReturn.kt
@@ -11,7 +11,24 @@ fun getShippingCostCalculator(delivery: Delivery): (Order) -> Double {
     return { order -> 1.2 * order.itemCount }
 }
 
+data class SiteVisit(val path: String, val duration: Double, val os: OS)
+
+enum class OS { WINDOWS, LINUX, MAC, IOS, ANDROID }
+
+val log = listOf(
+        SiteVisit("/", 34.0, OS.WINDOWS),
+        SiteVisit("/", 22.0, OS.MAC),
+        SiteVisit("/login", 12.0, OS.WINDOWS),
+        SiteVisit("/signup", 8.0, OS.IOS),
+        SiteVisit("/", 16.3, OS.ANDROID)
+)
+
+fun List<SiteVisit>.averageDurationFor(predicate: (SiteVisit) -> Boolean) = filter(predicate).map(SiteVisit::duration).average()
+
 fun main(args: Array<String>) {
     val calculator = getShippingCostCalculator(Delivery.EXPEDITED)
     println("Shipping costs ${calculator(Order(3))}")
+
+    println(log.averageDurationFor { it.os in setOf(OS.ANDROID, OS.IOS) })
+    println(log.averageDurationFor { it.os == OS.IOS && it.path == "/signup" })
 }
